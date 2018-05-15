b7585d48817de3ec3c09430d04d6195aac4e575d
Max <maximilian.schenke@googlemail.com>
2018-05-15 23:21:07 +0200

    css Navigation

--- index.php
+++ index.php
@@ -9,11 +9,23 @@
 		<div class="container-fluid">
 			<div class="row">
 			
-				<div class="col-md-2">
-					<h1>Admin<h1>
+				<div class="col-sm-2">
+					<h1>Admin</h1>
+					<ul id="side_menue" class="nav nav-pills nav-stacked">
+						<li class="active"><a href="dashboard.php">Dashboard</a></li>
+						<li><a href="#">Add New Post</a></li>
+						<li><a href="#">Categorie</a></li>
+						<li><a href="#">Manage Admins</a></li>
+						<li><a href="#">Comments</a></li>
+						<li><a href="#">Live Blog</a></li>
+						<li><a href="#">Logout</a></li>
+						
+
+					</ul>
 				</div>
 				
-				<div class="col-md-10">
+				
+				<div class="col-sm-10">
 					<h2>Hier entsteht das Dashboard</h2>
 					<p>Lorem ipsum ...</p>
 				</div>
