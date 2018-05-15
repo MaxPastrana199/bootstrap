2069779622344900ce21a0172f6e3febb6d83457
Max <maximilian.schenke@googlemail.com>
2018-05-15 16:30:29 +0200

    Änderungen index.php und style.css

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title></title>
+		<title>Admin Dashboard</title>
 		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
 	</head>
 	<body>
-	<?php echo "Hello World";?>
-	<h1>Hello World</h1>
+		<div class="container-fluid">
+			<div class="row">
+			
+				<div class="col-md-2">
+					<h1>Admin<h1>
+				</div>
+				
+				<div class="col-md-10">
+					<h2>Hier entsteht das Dashboard</h2>
+					<p>Lorem ipsum...</p>
+				</div>
+
+			</div>
+		</div>
+
+	
 	
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
