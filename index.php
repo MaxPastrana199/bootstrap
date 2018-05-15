8de8d68518bf08cc29f0c437f75b5b849992a72c
Max <maximilian.schenke@googlemail.com>
2018-05-15 15:55:36 +0200

    Grundgerüst Bootstrap

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+	</head>
+	<body>
+	<?php echo "Hello World";?>
+	<h1>Hello World</h1>
+	
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
