f2c01109fe44b275f3295ee1a668a304cef9b080
Max <maximilian.schenke@googlemail.com>
2018-05-16 16:14:46 +0200

    awesomefont für Sidebar und Gestaltung Footer

--- index.php
+++ index.php
@@ -4,34 +4,52 @@
 		<title>Admin Dashboard</title>
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+
 	</head>
 	<body>
 		<div class="container-fluid">
-			<div class="row">
-			
+			<div class="row">			
 				<div class="col-sm-2">
-					<h1>Admin</h1>
+					<h1 class="side_menue">Dashboard</h1>
 					<ul id="side_menue" class="nav nav-pills nav-stacked">
-						<li class="active"><a href="dashboard.php">Dashboard</a></li>
-						<li><a href="#">Add New Post</a></li>
-						<li><a href="#">Categorie</a></li>
-						<li><a href="#">Manage Admins</a></li>
-						<li><a href="#">Comments</a></li>
-						<li><a href="#">Live Blog</a></li>
-						<li><a href="#">Logout</a></li>
-						
-
+						<li class="active"><a href="dashboard.php"><span class="glyphicon glyphicon-th"></span> Dashboard</a></li>
+						<li><a href="#"><i class="fas fa-file-alt"></i> Add New Post</a></li>
+						<li><a href="#"><i class="fas fa-tags"></i> Categorie</a></li>
+						<li><a href="#"><i class="fas fa-user"></i> Manage Admins</a></li>
+						<li><a href="#"><i class="fas fa-comment"></i> Comments</a></li>
+						<li><a href="#"><i class="fas fa-desktop"></i> Live Blog</a></li>
+						<li><a href="#"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
 					</ul>
-				</div>
-				
-				
+				</div>								
 				<div class="col-sm-10">
+					<h1>Admin Dashboard</h1>
+					<h2>Hier entsteht das Dashboard</h2>
+					<p>Lorem ipsum ...</p>
+					<h2>Hier entsteht das Dashboard</h2>
+					<p>Lorem ipsum ...</p>
+					<h2>Hier entsteht das Dashboard</h2>
+					<p>Lorem ipsum ...</p>
+					<h2>Hier entsteht das Dashboard</h2>
+					<p>Lorem ipsum ...</p>
 					<h2>Hier entsteht das Dashboard</h2>
 					<p>Lorem ipsum ...</p>
 				</div>
-
 			</div>
 		</div>
+
+			<div id="footer">
+				<hr>
+				<p>Copyright by Max</p>
+				<ul id="footer" class="nav">
+					<li><a href="#">Impressum</a></li>
+					<li><a href="#">Datenschutz</a></li>
+				</ul>
+				<hr>
+				
+				
+			</div>
 
 	
 	
